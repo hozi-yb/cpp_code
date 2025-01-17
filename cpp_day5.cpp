949785278b32638b98240db6914792babefb3a64
hozi-yb <jerrygo009@gmail.com>
2025-01-17 19:17:44 +0900

    day5 수업 끝. 구조체, 파싱, 배열, 스트링 함수

--- cpp_day5.cpp
+++ cpp_day5.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <string>
-#include <typeinfo>
 
-#define MAIN5
+//#define MAIN5
 
 #ifdef MAIN5
 
@@ -10,18 +9,77 @@ using namespace std;
 
 int main()
 {
-	int grade[5] = { 1,2,3,4,5 };
+
+	/*
+	const int val = 5; // 학생의 명 수
+	// const : 얘를 변수로 보지 않고 상수로 보겠다는 뜻.
+	// 배열은 특성상 []에 변수가 들어가면 안된다.
+	// 그래서 int val로 선언을 해주면 배열 선언시 넣지 못한다.
+	
+	int grade[val] = { 1,2,3,4,5 }; // 배열의 크기를 설정해두면, 0으로 채워져있지만, 명시적으로 초기화해주는게 좋다.
 	float sum = 0;
 
-
-	for (int i = 1; i <= 5; i++)
+	for (int i = 1; i <= val; i++)
 	{
 	cout << i << "번 학생의 성적을 입력하세요: ";
 	cin >> grade[i - 1];
 
 	sum += grade[i-1];
 	}
-	cout << "성적 평균 : " << sum / 5;
+	cout << "성적 평균 : " << sum / val;
+	*/
+
+	
+	int exam[5][3] = { {101,}, };
+
+	string input = " ";
+	string space = " ";
+	
+	
+
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << i+1 << "번 학생의 (영어 수학 과학) 성적을 입력하시오. : " << endl;
+		std::cout << "(영어 수학 과학) 점수 사이는 스페이스바 " << endl;
+		getline(cin, input);
+		int point = 0;
+		
+
+		for (int k = 0; k < 3; k++)
+		{
+			int val = input.find(space, point);
+
+			string score = input.substr(point, val - point);
+
+			exam[i][k] = stoi(score);
+			point = val + 1;
+		}
+
+		
+		std::cout << endl;
+	}
+
+	std::cout << "===============================================" << endl;
+	std::cout << endl;
+
+
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << i+1 << "번 학생의 영어, 수학, 과학 성적은 : ";
+		for (int j = 0; j < 3; j++)
+		{
+			std::cout << exam[i][j] << " ";
+		}
+		std::cout << endl;
+	}
+
+	
+
+
+
+	return0;
+
+
 }
 
 #endif
---
+++ cpp_day5_2.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+
+#define MAIN5_2
+
+#ifdef MAIN5_2
+
+using namespace std;
+
+//구조체 선언은 전역변수에 선언하는것이 일반적이다.
+//struct Car { // 구조체 정의
+//	string name;
+//	int age;
+//	int door;
+//	float engine_temp[10];
+//};
+
+struct Rectangle {
+	float width;
+	float height;
+};
+
+using namespace std;
+
+int main()
+{
+	// 실습 20
+
+	Rectangle rect1;
+
+	string input;
+
+
+	cout << "가로, 세로 길이를 입력하세요. (가로 세로) : ";
+	cin >> rect1.width;
+	cin >> rect1.height;
+	// 스페이스바 기준으로 나눠짐;; 버퍼 때문에?? 버퍼에 담았다ㅏㄱ 짤랐다가 다음 변수에 집어넣는듯.
+	// flush를 이용해서 버퍼를 지우기도 한다.
+	int a = 0;
+
+	cin >> a;
+
+	cout << "넓이는 : " << rect1.width * rect1.height;
+	cout << endl << a; // a에 또 들어간다...
+
+
+
+
+	//Car c1 = {"KIA", 200, 2, 99.9}; // 밑과 동일하다.
+	
+	/*Car c1;
+
+	c1.name = "KIA";
+	c1.age = 2000;
+	c1.door = 2;
+	c1.engine_temp[0] = 99.9;
+
+	cout << c1.name << endl;
+	cout << c1.age << endl;
+	cout << c1.door << endl;
+	cout << c1.engine_temp[0] << endl;*/
+
+
+	return 0;
+}
+
+#endif
